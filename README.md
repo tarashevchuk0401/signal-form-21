3c8ab4975e5ebfad6868cf54b20862733cb9e97e
Taras Shevchuk <Taras.Shevchuk.extern@rwe.com>
2026-04-21 15:57:36 +0200

    delivery details

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SignalForm21
 ### For presentation use branch "foms-presentation"
+
+## More info here :
+https://angular-buch.com/blog/2025-10-signal-forms-part2
--- src/app/features/delivery/delivery.ts
+++ src/app/features/delivery/delivery.ts
@@ -1,25 +1,30 @@
 import { Component, inject, resource, signal } from '@angular/core';
 import {
   apply,
+  applyWhen,
   form,
   FormField,
-  FormRoot, minLength,
+  FormRoot,
+  hidden,
+  minLength,
   required,
   schema,
+  validate,
   validateAsync,
-  validateHttp,
   validateTree,
 } from '@angular/forms/signals';
 import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { FormError } from 'src/app/shared/components/form-error/form-error';
-import { SignUpDto } from 'src/app/shared/interfaces/sign-up.dto';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { RegistrationService } from 'src/app/shared/services/registration.service';
 import { JsonPipe } from '@angular/common';
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
+import { MatOption, MatSelect } from '@angular/material/select';
 
 export interface DeliveryForm {
   account: AccountForm;
+  deliveryDetails: DeliveryDetailsForm;
 }
 
 export interface AccountForm {
@@ -27,6 +32,20 @@ export interface AccountForm {
   email: string;
   password: string;
   confirmPassword: string;
+}
+
+export type DeliveryType = 'home' | 'pickup';
+export type DeliveryDay = 'Mon-Fri' | 'Sat-Sun';
+
+export interface DeliveryDetailsForm {
+  deliveryType: DeliveryType;
+  address: {
+    street: string;
+    city: string;
+    zip: string;
+  };
+  deliveryDay: DeliveryDay;
+  timeSlot: string;
 }
 
 export const AccountFormSchema = schema<AccountForm>((schema) => {
@@ -62,21 +81,60 @@ export const AccountFormSchema = schema<AccountForm>((schema) => {
 
   required(schema.confirmPassword, { message: 'Password confirmation is required' });
 
-  ///FIX
+  // ERROR. Should display error if password and confirmPassword fields are not equal
   validateTree(schema, (ctx) => {
     return ctx.value().password === ctx.value().confirmPassword
       ? undefined
       : {
-          field: ctx.fieldTree.confirmPassword, // assign the error to the second password field
+          field: ctx.fieldTree.confirmPassword,
           kind: 'confirmationPassword',
           message: 'The entered password must match with the one specified in "Password" field.',
         };
   });
 });
 
+export const DeliveryDetailFormSchema = schema<DeliveryDetailsForm>((schema) => {
+  required(schema.deliveryType, { message: 'Delivery type is required' });
+
+  hidden(schema.address, (ctx) => ctx.valueOf(schema.deliveryType) === 'pickup');
+  required(schema.address.city, { message: 'City is required' });
+  required(schema.address.zip, { message: 'Zip is required' });
+  minLength(schema.address.zip, 3, { message: 'Zip too short' });
+  required(schema.address.street, { message: 'Street is required' });
+
+  required(schema.timeSlot, { message: 'Time slot is required' });
+  applyWhen(
+    schema,
+    (ctx) => ctx.value().deliveryDay === 'Sat-Sun',
+    (pathWhenTrue) => {
+      validate(pathWhenTrue.timeSlot, (ctx) =>
+        ctx.value() !== '10'
+          ? {
+            kind: 'noTopicSelected',
+            message: 'Select another slot. Only 10:00 is available in weekend. ',
+          }
+          : undefined
+      );
+    }
+  );
+});
+
 @Component({
   selector: 'app-delivery',
-  imports: [FormRoot, MatFormField, MatLabel, MatInput, FormField, MatButton, FormError, JsonPipe],
+  imports: [
+    FormRoot,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    FormField,
+    MatButton,
+    FormError,
+    JsonPipe,
+    MatRadioGroup,
+    MatRadioButton,
+    MatSelect,
+    MatOption,
+  ],
   templateUrl: './delivery.html',
   styleUrl: './delivery.scss',
 })
@@ -88,22 +146,46 @@ export class Delivery {
       password: '',
       confirmPassword: '',
     },
+    deliveryDetails: {
+      deliveryType: 'pickup',
+      address: {
+        street: '',
+        city: '',
+        zip: '',
+      },
+      deliveryDay: 'Mon-Fri',
+      timeSlot: '',
+    },
   });
 
   deliveryForm = form(
     this.deliveryFormModel,
     (schemaPath) => {
       apply(schemaPath.account, AccountFormSchema);
+      apply(schemaPath.deliveryDetails, DeliveryDetailFormSchema);
     },
     {
       submission: {
         action: async () => {
-          console.log('is valid', this.deliveryForm().valid());
+          console.log('is valid', this.deliveryForm());
           this.openSnackBar('Form submitted successfully!', 'Close');
         },
       },
     },
   );
+
+  setAccountForm(): void {
+    this.deliveryFormModel.update((v) => ({
+      ...v,
+      account: {
+        ...v.account,
+        username: 'taras',
+        email: 'taras@gmail.com',
+        password: '123456',
+        confirmPassword: '123456',
+      },
+    }));
+  }
 
   private _snackBar = inject(MatSnackBar);
   private openSnackBar(message: string, action: string) {
