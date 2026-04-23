810a22842c094f08ea7eb7b7ac3f97d7557fc5a5
Taras Shevchuk <Taras.Shevchuk.extern@rwe.com>
2026-04-23 16:05:51 +0200

    refactored styles

--- src/app/features/delivery/delivery.ts
+++ src/app/features/delivery/delivery.ts
@@ -1,33 +1,17 @@
-import { Component, inject, resource, signal } from '@angular/core';
-import {
-  apply,
-  applyWhen,
-  form,
-  FormField,
-  FormRoot,
-  hidden,
-  minLength,
-  required,
-  schema,
-  validate,
-  validateAsync,
-  validateTree,
-} from '@angular/forms/signals';
-import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { Component, inject, signal } from '@angular/core';
+import { apply, form, FormRoot } from '@angular/forms/signals';
 import { MatButton } from '@angular/material/button';
-import { FormError } from 'src/app/shared/components/form-error/form-error';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { RegistrationService } from 'src/app/shared/services/registration.service';
-import { JsonPipe } from '@angular/common';
-import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
-import { MatOption, MatSelect } from '@angular/material/select';
 import {
   AccountForm,
   AccountFormSchema,
 } from 'src/app/features/delivery/account-form/account-form';
 import { AccountFormInterface } from 'src/app/shared/interfaces/forms/account-from.interface';
 import { DeliveryDetailsFormInterface } from 'src/app/shared/interfaces/forms/delivery-details-form.interface';
-import { DeliveryDetailFormSchema, DeliveryDetailsForm } from 'src/app/features/delivery/delivery-details-form/delivery-details-form';
+import {
+  DeliveryDetailFormSchema,
+  DeliveryDetailsForm,
+} from 'src/app/features/delivery/delivery-details-form/delivery-details-form';
 
 export interface DeliveryForm {
   account: AccountFormInterface;
@@ -37,26 +21,9 @@ export interface DeliveryForm {
 export type DeliveryType = 'home' | 'pickup';
 export type DeliveryDay = 'Mon-Fri' | 'Sat-Sun';
 
-
-
-
 @Component({
   selector: 'app-delivery',
-  imports: [
-    FormRoot,
-    MatFormField,
-    MatLabel,
-    MatInput,
-    FormField,
-    MatButton,
-    FormError,
-    MatRadioGroup,
-    MatRadioButton,
-    MatSelect,
-    MatOption,
-    AccountForm,
-    DeliveryDetailsForm,
-  ],
+  imports: [FormRoot, MatButton, AccountForm, DeliveryDetailsForm],
   templateUrl: './delivery.html',
   styleUrl: './delivery.scss',
 })
